25c554e84ba82abc5d567695dd1dc50ec5f7c7fc
olifog <oliverfogelin@gmail.com>
2024-11-30 23:59:36 +0000

    redirect to login if illegal onboarding access

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,9 +1,14 @@
 
 import { Onboarding } from "@/components/onboarding";
 import { getCurrentSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export default async function OnboardingPage() {
   const { user } = await getCurrentSession();
+
+  if (!user) {
+    return redirect("/login/google");
+  }
 
   return <Onboarding user={user} />;
 }
