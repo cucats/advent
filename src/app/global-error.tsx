c368282691d45c665c49bf73dcc970d3a6e256d8
olifog <oliverfogelin@gmail.com>
2024-12-01 00:18:25 +0000

    not found page

---
+++ src/app/global-error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  console.error(error);
+
+  return (
+    // global-error must include html and body tags
+    <html>
+      <body className="w-screen min-h-screen flex flex-col items-center bg-background text-zinc-300 pt-24">
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>{">"}Try again{"<"}</button>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <div className="w-screen min-h-screen flex flex-col items-center bg-background text-zinc-300 pt-24">
+      <h2>Page not found!</h2>
+    </div>
+  );
+}
