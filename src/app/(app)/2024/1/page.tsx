3b5c341f8f7f0d0a03e3f346d98022c023f924ea
olifog <oliverfogelin@gmail.com>
2024-12-01 13:10:22 +0000

    more backticks

--- src/app/(app)/2024/1/page.tsx
+++ src/app/(app)/2024/1/page.tsx
@@ -7,21 +7,31 @@ const questionOneMarkdown = String.raw`
 
 It was a peaceful December morning at the North Pole. Santa was sipping his gingerbread latte, with Mrs Claus knitting matching jumpers. Everything seemed perfect. Too perfect.
 
-As the countdown to December 25th began, the unthinkable happened. Somewhere deep inside the Holiday Algorithm, something went horribly wrong. Instead of neatly organising the Naughty/Nice lists and gift orders, the algorithm started spitting out _nonsense_:
+As the countdown to December 25th began, the unthinkable happened. Somewhere deep inside the Holiday Algorithm, something went horribly wrong.
+Instead of neatly organising the Naughty/Nice lists and gift orders, the algorithm started spitting out _nonsense_:
 
 * Little Timmy was scheduled to receive 41 left-foot socks.
 * Sophia was due to receive 10 tonnes of coal.
 * The GPS coordinates for Santa's sleigh were inexplicably set to the moon.
 
-The elves stared at the screens, horrified. "This... this can't be right!" said Tinker, the coding intern elf. No one dared to tell Santa. "Is this... a stack overflow?" "No," murmured Jingle, the head elf, trembling. "It's worse. **It's a heap of bugs.**"
+The elves stared at the screens, horrified. "This... this can't be right!" said Tinker, the coding intern elf.
+No one dared to tell Santa. "Is this... a stack overflow?" "No," murmured Jingle, the head elf, trembling. "It's worse. **It's a heap of bugs.**"
 
-The elves quickly turn to the only people they knew who could save Christmas. "There's got to be some intelligent life in Cambridge!" remarked Tinker. "For each bug you find, we'll reward you with **one star**!", said Holly, head of Elven Resources. "If you get **all twenty-five stars** by 25th December, we'll reward you greatly! And so, they send you off in a sleigh to the North Pole.
+The elves quickly turn to the only people they knew who could save Christmas. "There's got to be some intelligent life in Cambridge!" remarked Tinker.
+"For each bug you find, we'll reward you with **one star**!", said Holly, head of Elven Resources. "If you get **all twenty-five stars** by 25th December,
+we'll reward you greatly! And so, they send you off in a sleigh to the North Pole.
 
-Unfortunately, the sleigh's GPS is reading bogus numbers! Before taking off, the sleigh needs to be calibrated. The route planner has been loaded with a list of coordinates, but the destination (the last of the coordinates) isn't properly set. The North Pole should always be at \`(0, 0)\`, but right now it's off by some amount!
+Unfortunately, the sleigh's GPS is reading bogus numbers! Before taking off, the sleigh needs to be calibrated.
+The route planner has been loaded with a list of coordinates, but the destination (the last of the coordinates) isn't properly set.
+The North Pole should always be at \`(0, 0)\`, but right now it's off by some amount!
 
-To fix this, we need to adjust the entire route so the destination aligns with \`(0, 0)\`. For example, if the route loaded  in the GPS is [(4, -2), (3, 2), (1, 3), (-1, 1), (-3, -2)], then we must add (3, 2) to every coordinate to make the final destination, the North Pole, (0, 0). Here, after adding (3, 2) to every coordinate, we would get [(7, 0), (6, 4), (4, 5), (2, 3), (0, 0)]. For your answer, you should return the coordinate that is furthest away (by Euclidean Distance) from the North Pole, so in this example, you would return (6, 4).
+To fix this, we need to adjust the entire route so the destination aligns with \`(0, 0)\`. For example, if the route loaded  in the GPS is
+\`[(4, -2), (3, 2), (1, 3), (-1, 1), (-3, -2)]\`, then we must add \`(3, 2)\` to every coordinate to make the final destination, the North Pole, \`(0, 0)\`.
+Here, after adding \`(3, 2)\` to every coordinate, we would get \`[(7, 0), (6, 4), (4, 5), (2, 3), (0, 0)]\`. For your answer, you should return the coordinate
+that is furthest away (by Euclidean Distance) from the North Pole, so in this example, you would return \`(6, 4)\`.
 
-The input file will be in a particular format. The first line contains an integer, \`N\`. \`N\` lines follow, each containing two integers separated by a space, representing the \`x\` and \`y\` coordinates of the point.
+The input file will be in a particular format. The first line contains an integer, \`N\`. \`N\` lines follow, each containing two integers separated by
+a space, representing the \`x\` and \`y\` coordinates of the point.
 
 The answer should be given in the format \`(x, y)\`.
 `;
