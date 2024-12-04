662aecb18115bc0cbf26b7518e0c5aed162b2d04
olifog <oliverfogelin@gmail.com>
2024-12-04 11:56:52 +0000

    warning

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,6 +8,9 @@ export default function Layout({
   return (
     <div className="w-screen min-h-screen flex flex-col items-center bg-background text-zinc-300">
       <Header />
+      <div className="text-center text-zinc-100 bg-foreground p-4 rounded-lg border-2 border-highlight my-2">
+        Sorry, CompSci deadline management hit and we need another 30 mins - come back for D4 in a bit!
+      </div>
       {children}
     </div>
   );
