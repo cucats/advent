95c2ae37f2d6726542a5be635e5af6c19e93c9b5
olifog <oliverfogelin@gmail.com>
2024-12-10 11:57:10 +0000

    delay

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,6 +8,9 @@ export default function Layout({
   return (
     <div className="w-screen min-h-screen flex flex-col items-center bg-background text-zinc-300">
       <Header />
+      <div className="text-sm text-zinc-400 flex justify-center items-center w-full h-10 bg-orange-700 rounded-xl border-2 border-orange-500">
+        Day 10 is delayed 30 mins!
+      </div>
       {children}
     </div>
   );
