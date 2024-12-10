d2f177eeedbfbc593814600e3c49013dd45700de
olifog <oliverfogelin@gmail.com>
2024-12-10 12:05:28 +0000

    look prettier

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
   return (
     <div className="w-screen min-h-screen flex flex-col items-center bg-background text-zinc-300">
       <Header />
-      <div className="text-sm text-zinc-400 flex justify-center items-center w-full h-10 bg-orange-700 rounded-xl border-2 border-orange-500">
+      <div className="m-2 max-w-md text-sm text-zinc-100 flex justify-center items-center w-full h-10 bg-orange-700 rounded-xl border-2 border-orange-500">
         Day 10 is delayed 30 mins!
       </div>
       {children}
