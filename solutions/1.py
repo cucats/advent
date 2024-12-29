e6f1a2ea6ab31993522c9084df59db5e9bbce914
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-12-29 00:14:19 +0000

    Delete solutions directory

--- solutions/1.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-points = []
-for i in range(n):
-    x, y = map(int, input().split())
-    points.append((x, y))
-points = [(x - points[-1][0], y - points[-1][1]) for x, y in points]
-ans = max(points, key=lambda p: p[0] * p[0] + p[1] * p[1])
-print(ans)
