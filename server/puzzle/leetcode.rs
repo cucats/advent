b2097cdedd078ac87d81eb363902119deadf240c
IntGrah <acc@intgrah.com>
2025-07-18 14:54:33 +0100

    Delete server directory

--- server/puzzle/leetcode.rs
+++
@@ -1,9 +0,0 @@
-// pub trait Problem<I> {
-//     fn calculate_score(&self, submission: I) -> i32;
-// }
-
-// struct CodeSubmission {
-//     code: String,
-// }
-// type LeetcodeStyleProblem = dyn Problem<CodeSubmission>;
-
