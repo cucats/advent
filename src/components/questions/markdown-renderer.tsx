fcf96ba1082fdedcb0052bd3ef01447a8c091628
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-08-13 15:08:19 +0100

    Fix markdown-renderer.tsx

--- src/components/questions/markdown-renderer.tsx
+++ src/components/questions/markdown-renderer.tsx
@@ -8,34 +8,35 @@ import { LegacyRef } from "react";
 
 export const MarkdownRenderer = ({ children }: { children: string }) => {
   return (
-    <Markdown
-      remarkPlugins={[remarkMath]}
-      rehypePlugins={[rehypeKatex]}
-      className="prose max-w-none text-zinc-300 prose-strong:text-foreground prose-code:text-foreground prose-blockquote:text-zinc-400"
-      components={{
-        code(props) {
-          const { children, className, ...rest } = props;
-          const match = /language-(\w+)/.exec(className || "");
-          return match ? (
-            <SyntaxHighlighter
-              {...rest}
-              ref={rest.ref as LegacyRef<SyntaxHighlighter> | undefined}
-              PreTag="div"
-              language={match[1]}
-              style={atomDark}
-              customStyle={{ fontSize: "0.8rem" }}
-            >
-              {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code {...rest} className={className}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {children.replace(/\\`/gi, "`")}
-    </Markdown>
+    <div className="prose max-w-none text-zinc-300 prose-strong:text-foreground prose-code:text-foreground prose-blockquote:text-zinc-400">
+      <Markdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+        components={{
+          code(props) {
+            const { children, className, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                ref={rest.ref as LegacyRef<SyntaxHighlighter> | undefined}
+                PreTag="div"
+                language={match[1]}
+                style={atomDark}
+                customStyle={{ fontSize: "0.8rem" }}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {children.replace(/\\`/gi, "`")}
+      </Markdown>
+    </div>
   );
 };
